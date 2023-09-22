c7099180205bee678937c583833a05ab3979f432
frank gimeno <frankgimeno3@gmail.com>
2023-09-22 11:49:53 +0200

    actualizo preguntas

--- lib/quiz_brain.dart
+++ lib/quiz_brain.dart
@@ -4,30 +4,30 @@ class QuizBrain {
   int _questionNumber = 0;
 
   List<Question> _questionBank = [
-    Question('Some cats are actually allergic to humans', true),
-    Question('You can lead a cow down stairs but not up stairs.', false),
-    Question('Approximately one quarter of human bones are in the feet.', true),
-    Question('A slug\'s blood is green.', true),
-    Question('Buzz Aldrin\'s mother\'s maiden name was \"Moon\".', true),
-    Question('It is illegal to pee in the Ocean in Portugal.', true),
+    Question('Algunos gatos son alérgicos a los humanos', true),
+    Question('Las vacas no son capacs de andar hacia atrás.', false),
+    Question('Aproximadamente una cuarta parte de los huesos humanos están en los pies', true),
+    Question('La sangre de una babosa es verde', true),
+    Question('El nombre real de \'El Fary \' era \" José Monge Cruz \".', true),
+    Question('La capital de Somalia es Mogadishu.', true),
     Question(
-        'No piece of square dry paper can be folded in half more than 7 times.',
+        'Es imposible doblar una sevilleta más de 20 veces sobre sí misma.',
         false),
     Question(
-        'In London, UK, if you happen to die in the House of Parliament, you are technically entitled to a state funeral, because the building is considered too sacred a place.',
+        'En algunos estados de EEUU, puedes conducir motocicletas sin usar casco',
         true),
     Question(
-        'The loudest sound produced by any animal is 188 decibels. That animal is the African Elephant.',
+        'Con 188 decibelios, el elefante africano es el animal capaz de realizar un sonido más fuerte',
         false),
     Question(
-        'The total surface area of two human lungs is approximately 70 square metres.',
+        'La serpiente más venenosa es la serpiente taipán (Oxyuranus microlepidotus).',
         true),
-    Question('Google was originally called \"Backrub\".', true),
+    Question('Google inicialmente se llamaba \"Backrub\".', true),
     Question(
-        'Chocolate affects a dog\'s heart and nervous system; a few ounces are enough to kill a small dog.',
+        'Es posible envenenar a un perro con chocolate.',
         true),
     Question(
-        'In West Virginia, USA, if you accidentally hit an animal with your car, you are free to take it home to eat.',
+        'El aguacate es comestible para los humanos, pero tóxico para la mayoría de nuestras mascotas.',
         true),
   ];
 
